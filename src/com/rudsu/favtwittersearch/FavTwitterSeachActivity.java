79f9d64cabe7684a5e585179259b72e77624f656
Rudy Suyanto <free4spams@yahoo.com>
2012-04-15 21:45:11 +0700

    first commit

---
+++ src/com/rudsu/favtwittersearch/FavTwitterSeachActivity.java
@@ -0,0 +1,35 @@
+package com.rudsu.favtwittersearch;
+
+import java.util.Arrays;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+
+public class FavTwitterSeachActivity extends Activity {
+	private SharedPreferences savedSearches; // user's fav searches
+	private TableLayout queryTableLayout; // show search button
+	private EditText queryEditText; // where user enter queries
+	private EditText tagEditText; // where user enter a query tag
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
